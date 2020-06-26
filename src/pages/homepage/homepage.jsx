a0df88bb985e1eb6d0ef8eeb857a177682840a14
sacad786 <sacad786@gmail.com>
2020-06-26 12:47:32 +0200

    broken the homepage into smaller components and finished styling the menu-item

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homepage.scss'
+import Directory from '../../components/directory/directory'
+export const Homepage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+
